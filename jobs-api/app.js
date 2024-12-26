7d93285a4ef668e4297b7c37b608629cffdda7ef
Sai <vsaisumanth241196@gmail.com>
2024-12-27 00:46:23 +0530

    completed code

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
 const app = express()
+
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
 //connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
@@ -16,6 +22,16 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
 // extra packages
+app.set('trust proxy', 1)
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
--- jobs-api/controllers/jobs.js
+++ jobs-api/controllers/jobs.js
@@ -1,19 +1,72 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ jobs: job })
+}
+
 const getAllJobs = async (req, res) => {
-  res.send('All jobs route')
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+  res.status(StatusCodes.OK).json({ jobs: jobs })
 }
 
 const getSingleJob = async (req, res) => {
-  res.send('Get single route')
+  const { userId } = req.user
+  const { id: jobId } = req.params
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ jobs: job })
 }
 
 const updateJob = async (req, res) => {
-  res.send('Edit job route')
+  const { user: userId } = req
+  const {
+    params: { id: jobId },
+  } = req
+
+  const { company, position } = req.body
+
+  if (!company === '' || !position === '') {
+    throw new BadRequestError('Please provide all the details')
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ jobs: job })
 }
-const createJob = async (req, res) => {
-  res.json(req.user)
-}
+
 const deleteJob = async (req, res) => {
-  res.send('Delete job route')
+  const { user: userId } = req
+  const {
+    params: { id: jobId },
+  } = req
+
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ msg: 'Job deleted successfully' })
 }
 
 module.exports = {
