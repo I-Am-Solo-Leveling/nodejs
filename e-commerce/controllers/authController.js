e8a9aca03de78609145fc2db01e0803a6137790d
Sai <vsaisumanth241196@gmail.com>
2024-12-30 16:15:46 +0530

    completed jwt refactor for cookies

--- e-commerce/controllers/authController.js
+++ e-commerce/controllers/authController.js
@@ -1,5 +1,26 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { createJWT } = require('../utils');
+
 const registerUser = async (req, res) => {
-  res.send('Register user Route');
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Please enter a valid value');
+  }
+
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 
 const loginUser = async (req, res) => {
